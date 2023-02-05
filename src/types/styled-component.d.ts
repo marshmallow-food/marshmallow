884379c286bf352e2340ddbfcc338388d787565d
rodionrostovshchikov <r.rostovshchikov@indriver.com>
2023-02-06 02:54:44 +0600

    Add Sentry

--- src/types/styled-component.d.ts
+++ src/types/styled-component.d.ts
@@ -1,7 +1,7 @@
-import {} from 'styled-components';
 import {DefaultTheme as AppTheme} from 'src/theme';
 
 declare module 'styled-components' {
   type Theme = typeof AppTheme;
+  /* eslint-disable  @typescript-eslint/no-empty-interface */
   export interface DefaultTheme extends Theme {}
 }
